40ced7d18f082397672ac66b073b50879b6de9d3
Kosta Shumakov <shumakovk@i.opw.ie>
2018-10-19 12:24:18 +0100

    Remove mysql stuff. enable service and set password

--- db.sh
+++ db.sh
@@ -10,35 +10,10 @@ wget https://download.postgresql.org/pub/repos/yum/9.5/redhat/rhel-7-x86_64/pgdg
 sudo rpm -ivh pgdg-centos95-9.5-3.noarch.rpm
 sudo yum -y update
 sudo yum install -y postgresql95-server.x86_64
-sudo systemctl start postgresql
+sudo mv /usr/pgsql-9.5/share/pg_hba.conf.sample /var/lib/pgsql/9.5/pg_hba.conf
+sudo systemctl start postgresql-9.5.service
+sudo systemctl enable postgresql-9.5.service
 
 su -c "createuser otrs" -s /bin/bash postgres
+su -c "echo \"alter user otrs with encrypted password 'otrs-ioa';\"|psql "
 su -c "createdb otrs -O otrs" -s /bin/bash postgres
-
-mysql --user=root <<_EOF_
-UPDATE mysql.user SET Password=PASSWORD('otrs-ioa') WHERE User='root';
-DELETE FROM mysql.user WHERE User='';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-FLUSH PRIVILEGES;
-_EOF_
-
-echo "Setting max_allowed_packets"
-sed 's/^\[mysqld\]/\[mysqld\]\nmax_allowed_packet=100M/' /etc/my.cnf > /tmp/server.cnf
-cp /tmp/server.cnf /etc/my.cnf
-
-sed 's/^\[mysqld\]/\[mysqld\]\nskip-character-set-client-handshake/' /etc/my.cnf > /tmp/server.cnf
-cp /tmp/server.cnf /etc/my.cnf
-
-sed 's/^\[mysqld\]/\[mysqld\]\ncollation-server=utf8_unicode_ci/' /etc/my.cnf > /tmp/server.cnf
-cp /tmp/server.cnf /etc/my.cnf
-
-sed 's/^\[mysqld\]/\[mysqld\]\ncharacter-set-server=utf8/' /etc/my.cnf > /tmp/server.cnf
-cp /tmp/server.cnf /etc/my.cnf
-
-sed 's/^\[mysqld\]/\[mysqld\]\ninnodb_log_file_size=256M/' /etc/my.cnf > /tmp/server.cnf
-cp /tmp/server.cnf /etc/my.cnf
-
-sudo systemctl restart mysqld
-sudo systemctl enable mysqld
