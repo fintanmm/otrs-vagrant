9f9d068c97663b27bcdc6e8f75f60cb1549d8888
Fintan MacMahon <fintan.macmahon@gmail.com>
2018-12-13 15:29:28 +0000

    Set full path for daemon.pl

--- otrs.sh
+++ otrs.sh
@@ -1,3 +1,5 @@
+# VERSION="6.0.6" # OTRS
+# INSTALL_DIR="/data/www/otrs-$VERSION"
 echo "installing requirements for make"
 yum install -y lndir
 yum install -y perl-App-cpanminus.noarch
@@ -12,9 +14,11 @@ cp  ./etc/config.pl.dist ./etc/config.pl
 cd /home/vagrant
 sudo cp config.pl /opt/module-tools/etc
 
-su -c "./otrs.Daemon.pl start" -s /bin/bash otrs
+# su -c "$INSTALL_DIR/bin/otrs.Daemon.pl start" -s /bin/bash otrs
+su -c "/opt/otrs/bin/otrs.Daemon.pl start" -s /bin/bash otrs
 
 
-echo "go to: http://localhost:3002/otrs/installer.pl"
+# echo "go to: http://localhost:3002/otrs/installer.pl"
+echo "go to: http://localhost:3002/otrs/index.pl"
 echo "Install Settings -> usr: root password: otrs-ioa"
 echo "Vagrant VM credentials -> usr: vagrant password: vagrant"
